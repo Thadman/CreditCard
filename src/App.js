f630d0e4aca526c92cb53ff4240ac633ed6edddc
Thad Hoskin <thad.hoskin-01@gmail.com>
2021-01-20 11:59:26 +1100

    just playing around with the stlying, lots more to do

--- src/App.js
+++ src/App.js
@@ -31,51 +31,78 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="background">
-          <div className="form">
-            <form>
+          <div
+            className="form"
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            <form
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+              }}
+            >
               <Form
                 details={details}
                 query={query}
                 handleSubmit={handleSubmit}
               />
-              <div>
+              <div style={{ textAlign: "left", paddingLeft: "75px" }}>
                 <label> Card Number</label>
               </div>
-              <input
-                type="text"
-                placeholder="Type your message here"
-                value={query}
-                onChange={(event) => setQuery(event.target.value)}
-              />
               <div>
+                <input
+                  type="text"
+                  placeholder="Type your message here"
+                  value={query}
+                  onChange={(event) => setQuery(event.target.value)}
+                  style={{ width: "80%", height: "30px", fontSize: "25px" }}
+                />
+              </div>
+              <div style={{ textAlign: "left", paddingLeft: "75px" }}>
                 <label>Customer Name</label>
               </div>
-              <input
-                type="text"
-                placeholder="Type your message here"
-                value={query}
-                onChange={(event) => setQuery(event.target.value)}
-              />
               <div>
-                <label>Month</label>
-                <select>
-                  <option value="0">Month</option>
-                  <option value="1">Jan</option>
-                  <option value="2">Feb</option>
-                  <option value="3">Mar</option>
-                  <option value="4">April</option>
-                  <option value="5">May</option>
-                </select>
+                <input
+                  type="text"
+                  placeholder="Type your message here"
+                  // value={query}
+                  onChange={(event) => setQuery(event.target.value)}
+                  style={{ width: "80%", height: "30px", fontSize: "25px" }}
+                />
               </div>
-              <div>
-                <label>Year</label>
-                <select>
-                  <option value="0">Year</option>
-                  <option value="1">2021</option>
-                  <option value="2">2022</option>
-                  <option value="3">2023</option>
-                  <option value="4">2024</option>
-                </select>
+              <div
+                className="labels"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <div>
+                  <label>Month</label>
+                  <select>
+                    <option value="0">Month</option>
+                    <option value="1">Jan</option>
+                    <option value="2">Feb</option>
+                    <option value="3">Mar</option>
+                    <option value="4">April</option>
+                    <option value="5">May</option>
+                  </select>
+                </div>
+                <div>
+                  <label>Year</label>
+                  <select>
+                    <option value="0">Year</option>
+                    <option value="1">2021</option>
+                    <option value="2">2022</option>
+                    <option value="3">2023</option>
+                    <option value="4">2024</option>
+                  </select>
+                </div>
+                <div>
+                  <label>CVV</label>
+                  <input type="text" />
+                </div>
               </div>
             </form>
           </div>
